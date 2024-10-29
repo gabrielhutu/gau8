46f50b7c8c2c0268f79e32ce43b0680bb1943e6c
Gabriel Hutu <gabrielcristianhutu@gmail.com>
2024-10-29 14:01:49 +0200

    Added building script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -d ./build ];then
+    rm -rf ./build/
+fi
+
+if [ $EUID -eq 0 ];then 
+    apt update && apt install cmake make clang
+fi
+
+mkdir build;
+cmake -S ./ -B ./build;
+cd build; 
+make
