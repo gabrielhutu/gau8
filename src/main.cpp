eaf89816f6645f21cb26d958a66556cfbf90b679
Gabriel Hutu <gabrielcristianhutu@gmail.com>
2024-10-29 14:01:49 +0200

    added some comments

--- src/main.cpp
+++ src/main.cpp
@@ -40,15 +40,18 @@ int main(int argc, char** argv)
 
     for(uint16_t i = 1; i <= atoi(argv[4]); i++)
     {
+        //add all threads to the vector of threads 
         std::cout << "Initializing thread " << i;
         threads.push_back(new std::thread([&]{
             char f_password[15];
             while(true)
             {
+                //basically create a new ssh connection and session, try 3 passwords, then kill it
                 int handle;
                 gau8::ssh* f_ssh = new gau8::ssh(argv[1], 22, handle);
                 while(handle)
                 {
+                    //some ssh servers might refuse connections, so block the thread until 
                     delete f_ssh; 
                     f_ssh = new gau8::ssh(argv[1], 22, handle);
                     std::this_thread::sleep_for(std::chrono::milliseconds(250));
@@ -57,16 +60,20 @@ int main(int argc, char** argv)
                 {
                     if(m_lock_pass.try_lock())
                     {
+                        //Lock the wordlist and read one password
                         wordlist >> f_password;
                         m_lock_pass.unlock();
                     }
+                    //try to authenticate via the username that the connection was created with and the password that was read above
                     if(f_ssh->auth_user_pass(argv[2], f_password))
                     {
+                        //raise the counter for the number of attempts
                         if(m_lock_pass.try_lock())
                         {
                             attempt_counter++;
                             m_lock_pass.unlock();
                         }
+                        //print the successful attempt
                         std::cout << "ATTEMPT " << attempt_counter << " - Auth successful for user " << argv[2] << ":" << f_password << std::endl;
                     }else
                     {
@@ -75,6 +82,7 @@ int main(int argc, char** argv)
                             attempt_counter++;
                             m_lock_pass.unlock();
                         }
+                        //print the unsuccessful attempt
                         std::cout << "ATTEMPT " << attempt_counter << " - Auth failed for user " << argv[2] << ":" << f_password << std::endl;
                     }
                 }
